56a589b0dcd40ca98b11430198e228dbc8c777ce
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-11 21:11:06 -0400

    attempting to connect front and back-end app.

--- api/register/registerRouter.js
+++ api/register/registerRouter.js
@@ -10,9 +10,9 @@ router.post('/', routerMiddleware.checkUser, async (req, res) => {
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash;
     try {
-        console.log(user);
-        const registeredUser = await appUsers.add(user);
-        res.status(201).json(registeredUser);
+        // console.log(user);
+        // const registeredUser = await appUsers.add(user);
+        res.status(201).json(user);
         // res.status(201).json({ email, password });
 
     } catch (err) {
