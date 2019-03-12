e7c2345a6a264c153547f41061b47edb7b632ee3
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-11 20:22:58 -0400

    Attempting to change port to connect to front-end.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, () => {
     console.log(`\n*** Listening on port ${port} ***\n`);
